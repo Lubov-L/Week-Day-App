c4c6ffee0caccfe8e94e5669b528742c0525ae27
Lubov <lb.librab14@gmail.com>
2022-04-28 23:43:53 +0300

    Add all constraints

--- Week Day App/ViewController.swift
+++ Week Day App/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var dateText: UITextField!
+    @IBOutlet weak var monthText: UITextField!
+    @IBOutlet weak var yearText: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func findDay() {
+        
     }
-
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 }
 
